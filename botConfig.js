848f74ac71fd6819a1018dcc6a6b1c6294d20f39
Thanbunga <95388117+Thanbunga@users.noreply.github.com>
2025-04-22 12:31:36 -0400

    Initial

---
+++ botConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+    token: "MTExNTc5NDA4MDEyMzg2MzEzMw.GAn00S.PGXjp_RTaRxsu9twwb7a9ZLooJwqoBQVQP0nBo",
+    prefix: "/",
+    admins: [
+        "642337787869265952"
+],
+    debug: true,
+    countChannel: ""
+};
